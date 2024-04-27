190a977a47a311451909b3f9e7c39675bc3afef8
tayoshi-tokisada <mail.by.blacksheep@gmail.com>
2024-04-27 18:07:40 +0900

    change copying トップボタン、モーダルの✖ボタン、フェードの調整

--- kadai_copying/ka-profile/script.js
+++ kadai_copying/ka-profile/script.js
@@ -9,12 +9,34 @@ $(function(){
     fade: true,
   });
 
-  // 画像オーバーレイ
+  // 画像モーダルオープン
   $(".clickImg").on("click", function(){
-    let src = $(".clickImg").attr("src");
-    $("#overlay1").fadeIn();
+    let src = $(this).attr("src");
+    $("#modal").attr("src", src);
+    $("#overlay").fadeIn();
+  });
+  // 画像モーダルクローズ
+  $("#modalClose").on("click", function(){
+    $("#overlay").fadeOut();
+  });
 
-    console.log(src);
-    console.log();
+  // トップボタンクリック
+  $("#topBtn").on("click", function(){
+    scrollTo({
+      top: $("header").position().top,
+      left: $("header").position().left,
+      behavior: "smooth",
+    });
+  });
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() >= 100){
+      $("#backToTop").css("visibility", "visible");
+      $("#backToTop").css("opacity", 1);
+    }
+    else{
+      $("#backToTop").css("opacity", 0);
+      $("#backToTop").css("visibility", "hidden");
+    }
   });
 });
